20cf495339300345d2e341b03ae7822bd1ef917a
Megan Ostrander <Megan@DESKTOP-G32KF0T>
2021-02-22 18:59:55 -0500

    Simply adding the first version of my final project to GitHub so I do not lose any changes.

---
+++ Final Project Files/Ciss-222-Final_Project_Initial_ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ciss_222_Final_Project_Initial_ConsoleApp
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
